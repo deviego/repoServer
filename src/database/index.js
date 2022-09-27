79fa791ba20f0bb1d76f27fd73c02c31d3904735
Deviego <deviego4@gmail.com>
2022-09-27 17:44:40 -0300

    database conected

--- src/database/index.js
+++ src/database/index.js
@@ -4,10 +4,7 @@ import config from "../config/database"
 
 class Database {
     constructor(){
-        this.connection = mongoose.connect(config.url, {
-            useNetUrlParser:true,
-            UseUnifiedTopology:true
-        })
+        this.connection = mongoose.connect(config.url, {})
     }
 }
 
