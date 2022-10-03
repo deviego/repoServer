61cd5ccd24ac1e4b55414ea49daf80529a43741e
Deviego <deviego4@gmail.com>
2022-10-03 15:10:19 -0300

    repo controller completed

--- src/controllers/UserControllers.js
+++ src/controllers/UserControllers.js
@@ -1,4 +1,4 @@
-import User from "../models/user";
+import User from "../models/User";
 import { createPasswordHash } from "../services/auth";
 
 class UserController {
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import  express  from 'express'
 import helloController from './controllers/helloController'
 import UserController from './controllers/UserControllers'
-
+import RepoController from './controllers/RepoController'
 
 const routes =  express()
 
@@ -12,6 +12,10 @@ routes.get('/users/:id', UserController.show)
 routes.post('/users', UserController.create)
 routes.put('/users/:id', UserController.update)
 routes.delete('/users/:id', UserController.destroy)
+
+routes.get('/users/:user_id/repositories', RepoController.index)
+routes.post('/users/:user_id/repositories', RepoController.create)
+routes.delete('/users/:user_id/:id', RepoController.destroy)
 
 
 
